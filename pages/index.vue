6c480be549fd08f6ddb820e0f7db487515577b83
Abdulbasit <abdulbasitsaeed619@gmail.com>
2020-12-14 07:22:06 +0100

    config

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="p-6">
+    <create-account></create-account>
+    <!-- main -->
+    <section class="flex flex-col p-20 w-96 justify-center">
+      <div class="bg-green-400">
+        <div class="text-center">
+          <div class="text-3xl font-bold">Hello, Welcome Back</div>
+          <p class="text-sm">Please fill out the following fields to login</p>
+        </div>
+        <div class="rounded p-4 bg-white">Login with google</div>
+      </div>
+    </section>
+    <!-- main -->
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
