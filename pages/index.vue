9705ff6383c937d84608950a786171b8ca5209be
Abdulbasit <abdulbasitsaeed619@gmail.com>
2020-12-15 20:36:46 +0100

    added svgs

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,32 @@
 <template>
-  <div>
-
+  <div class="min-h-screen">
+    <!-- first -->
+    <div class="flex items-center justify-center h-full">
+      <div class="flex flex-col my-24 justify-center items-center gap-10">
+        <div class="text-2xl">Searching For something? Try Stockcheck</div>
+        <div>We help you find tools that help your business in no time</div>
+        <div
+          class="flex items-center w-full rounded border py-3 px-6 justify-between"
+        >
+          <img src="/icons/search.svg" alt="" />
+          <input type="text" name="" id="" class="w-full border-none" />
+          <div class="flex items-center gap-2">
+            <img src="/icons/filter.svg" alt="" />
+            <span class="text-primary">Filter</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- end -->
+    <!-- second -->
+    <div class="border-t"></div>
+    <!-- end -->
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
