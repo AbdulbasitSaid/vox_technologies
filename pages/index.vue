533f1250c8f8374710dd7fbb3bd51eab8478a150
Abdulbasit <abdulbasitsaeed619@gmail.com>
2020-12-22 17:16:29 +0100

    added main

--- pages/index.vue
+++ pages/index.vue
@@ -1,52 +1,57 @@
 <template>
-  <div class="min-h-screen">
-    <!-- first -->
-    <div class="flex items-center justify-center h-full">
-      <div class="flex flex-col my-24 justify-center items-center gap-10">
-        <div class="text-2xl">Searching For something? Try Stockcheck</div>
-        <div>We help you find tools that help your business in no time</div>
-        <div
-          class="flex items-center w-full rounded border py-3 px-6 justify-between"
-        >
-          <img src="/icons/search.svg" alt="" />
-          <input type="text" name="" id="" class="w-full border-none" />
-          <div class="flex items-center gap-2">
-            <img src="/icons/filter.svg" alt="" />
-            <span class="text-primary">Filter</span>
+  <div>
+    <main>
+      <div class="min-h-screen">
+        <!-- first -->
+        <div class="flex items-center justify-center h-full">
+          <div class="flex flex-col my-24 justify-center items-center gap-10">
+            <div class="text-2xl">Searching For something? Try Stockcheck</div>
+            <div>We help you find tools that help your business in no time</div>
+            <div
+              class="flex items-center w-full rounded border py-3 px-6 justify-between"
+            >
+              <img src="/icons/search.svg" alt="" />
+              <input type="text" name="" id="" class="w-full border-none" />
+              <div class="flex items-center gap-2">
+                <img src="/icons/filter.svg" alt="" />
+                <span class="text-primary">Filter</span>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-    <!-- end -->
-    <!-- second -->
-    <div class="border-t flex items-center py-10 px-12 gap-6">
-      <div class="flex flex-col gap-2">
-        <div><img src="/icons/search_green.svg" alt="" /></div>
-        <div>Search Anything</div>
-        <div>Hard to find ? try searching for it on our platform.</div>
-      </div>
-      <div class="flex flex-col gap-2">
-        <div><img src="/icons/laptop.svg" alt="" /></div>
-        <div>Easy Sign Up</div>
-        <div>
-          Our registration process is as seamless as our goal is to help you
-          reach your goal.
+        <!-- end -->
+        <!-- second -->
+        <div class="border-t flex items-center py-10 px-12 gap-6">
+          <div class="flex flex-col gap-2">
+            <div><img src="/icons/search_green.svg" alt="" /></div>
+            <div>Search Anything</div>
+            <div>Hard to find ? try searching for it on our platform.</div>
+          </div>
+          <div class="flex flex-col gap-2">
+            <div><img src="/icons/laptop.svg" alt="" /></div>
+            <div>Easy Sign Up</div>
+            <div>
+              Our registration process is as seamless as our goal is to help you
+              reach your goal.
+            </div>
+          </div>
+          <div class="flex flex-col gap-2">
+            <div><img src="/icons/quote.svg" alt="" /></div>
+            <div>Send Quote</div>
+            <div>Send direct messages to merchant for pricing.</div>
+          </div>
+          <div class="flex flex-col gap-2">
+            <div><img src="/icons/card.svg" alt="" /></div>
+            <div>Make Payment</div>
+            <div>
+              Pay merchant easily and track delivery to your desired
+              destination.
+            </div>
+          </div>
         </div>
+        <!-- end -->
       </div>
-      <div class="flex flex-col gap-2">
-        <div><img src="/icons/quote.svg" alt="" /></div>
-        <div>Send Quote</div>
-        <div>Send direct messages to merchant for pricing.</div>
-      </div>
-      <div class="flex flex-col gap-2">
-        <div><img src="/icons/card.svg" alt="" /></div>
-        <div>Make Payment</div>
-        <div>
-          Pay merchant easily and track delivery to your desired destination.
-        </div>
-      </div>
-    </div>
-    <!-- end -->
+    </main>
   </div>
 </template>
 
--- pages/sentreset.vue
+++ pages/sentreset.vue
@@ -1,50 +1,56 @@
 <template>
-  <div class="p-6">
-    <!-- <create-account></create-account> -->
-    <!-- main -->
-    <section class="flex flex-col p-20 justify-center items-center gap-6">
-      <div class="gap-10 flex flex-col w-96">
-        <div
-          class="text-center p-2 bg-primary border-l-4 border-primary bg-opacity-5"
-        >
-          We have sent instructions on how to reset your password to your email
-        </div>
-        <div class="">
-          <div class="text-3xl font-bold">Reset your password</div>
-          <p class="text-sm">
-            Enter your email and we will send you a link to reset your password.
-          </p>
-        </div>
+  <div>
+    <main>
+      <div class="p-6">
+        <!-- <create-account></create-account> -->
+        <!-- main -->
+        <section class="flex flex-col p-20 justify-center items-center gap-6">
+          <div class="gap-10 flex flex-col w-96">
+            <div
+              class="text-center p-2 bg-primary border-l-4 border-primary bg-opacity-5"
+            >
+              We have sent instructions on how to reset your password to your
+              email
+            </div>
+            <div class="">
+              <div class="text-3xl font-bold">Reset your password</div>
+              <p class="text-sm">
+                Enter your email and we will send you a link to reset your
+                password.
+              </p>
+            </div>
 
-        <!-- email adderess -->
-        <div class="text-sm font-thin">
-          <div class="flex flex-col">
-            Email address
-            <input
-              type="text"
-              name=""
-              id=""
-              class="border p-2 rounded w-full block"
-              placeholder="abdulbasitsaeed619@gmail.com"
-            />
+            <!-- email adderess -->
+            <div class="text-sm font-thin">
+              <div class="flex flex-col">
+                Email address
+                <input
+                  type="text"
+                  name=""
+                  id=""
+                  class="border p-2 rounded w-full block"
+                  placeholder="abdulbasitsaeed619@gmail.com"
+                />
+              </div>
+            </div>
+            <!-- end -->
+
+            <div class="w-full">
+              <nuxt-link
+                to="sentreset"
+                class="bg-primary py-2 px-6 rounded text-white w-full block text-center"
+                >Send Instructions</nuxt-link
+              >
+            </div>
+            <div class="gap-2 flex justify-center">
+              I remember my password
+              <nuxt-link to="/" class="text-primary"> Try Login </nuxt-link>
+            </div>
           </div>
-        </div>
-        <!-- end -->
-
-        <div class="w-full">
-          <nuxt-link
-            to="sentreset"
-            class="bg-primary py-2 px-6 rounded text-white w-full block text-center"
-            >Send Instructions</nuxt-link
-          >
-        </div>
-        <div class="gap-2 flex justify-center">
-          I remember my password
-          <nuxt-link to="/" class="text-primary"> Try Login </nuxt-link>
-        </div>
+        </section>
+        <!-- main -->
       </div>
-    </section>
-    <!-- main -->
+    </main>
   </div>
 </template>
 
